54542324a87b71f657047b73bfad8a79acb4c6ad
qriscilla <pri.whang@gmail.com>
2021-06-06 22:42:53 -0500

    First commit

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            //// Test for a negative balance
+            //try
+            //{
+            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to steal");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //// Test that the initial balances must be positive
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance.");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
